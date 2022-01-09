42e04e8e6252e6c9da9d0e132c16cbaa83e7a59d
Georghus <83593389+Georghus@users.noreply.github.com>
2022-01-09 23:13:35 +0530

    Linear Regression Excersises

--- Week 2/computeCost.m
+++ Week 2/computeCost.m
@@ -12,7 +12,6 @@ J = 0;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
-
 for i=1:m
   J= J +(((theta(1) + theta(2).*X(i,2) - y(i)).^2 ) / (2*m));
 endfor
